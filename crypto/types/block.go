2403548902e9e47570f43e721e47bfc7d23654de
LarsDM <larsdemaere@hotmail.com>
2023-03-20 06:33:54 +0100

    Sign blocks & Tests

--- crypto/types/block.go
+++ crypto/types/block.go
@@ -3,9 +3,14 @@ package types
 import (
 	"crypto/sha256"
 
+	"github.com/LarsDMsoftware/GoBlocker/crypto"
 	"github.com/LarsDMsoftware/GoBlocker/proto"
 	pb "github.com/golang/protobuf/proto"
 )
+
+func SignBlock(pk *crypto.PrivateKey, b *proto.Block) *crypto.Signature {
+	return pk.Sign(HashBlock(b))
+}
 
 // HashBlock returns a SHA256 of the header
 func HashBlock(block *proto.Block) []byte {
--- crypto/types/block_test.go
+++ crypto/types/block_test.go
@@ -3,7 +3,8 @@ package types
 import (
 	"testing"
 
-	"example.com/m/v2/crypto/util"
+	"github.com/LarsDMsoftware/GoBlocker/crypto"
+	"github.com/LarsDMsoftware/GoBlocker/util"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -11,4 +12,17 @@ func TestHashBlock(t *testing.T) {
 	block := util.RandomBlock()
 	hash := HashBlock(block)
 	assert.Equal(t, 32, len(hash))
+}
+
+func TestSignBlock(t *testing.T) {
+	var (
+		block   = util.RandomBlock()
+		privKey = crypto.GeneratePrivateKey()
+		pubkey  = privKey.Public()
+	)
+
+	sig := SignBlock(privKey, block)
+	assert.Equal(t, 64, len(sig.Bytes()))
+	assert.True(t, sig.Verify(pubkey, HashBlock(block)))
+
 }
