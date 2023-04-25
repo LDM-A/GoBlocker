d304c141fb0e5d9490cdb8232b10a9cfb0fd72d1
LarsDM <larsdemaere@hotmail.com>
2023-04-25 07:29:44 +0200

    putting,getting of blocks & some sanity checks

--- node/chain_test.go
+++ node/chain_test.go
@@ -1,1 +1,21 @@
 package node
+
+import (
+	"testing"
+
+	"github.com/LarsDMsoftware/GoBlocker/types"
+	"github.com/LarsDMsoftware/GoBlocker/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddBlock(t *testing.T) {
+	bs := NewMemoryBlockStore()
+	chain := NewChain(bs)
+	block := util.RandomBlock()
+	blockHash := types.HashBlock(block)
+	assert.Nil(t, chain.AddBlock(block))
+
+	fetchedBlock, err := chain.GetBlockByHash(blockHash)
+	assert.Nil(t, err)
+	assert.Equal(t, block, fetchedBlock)
+}
